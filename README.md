e810a4b6a87cc5edd7b9154c1e0571ff0acb0cfa
https://github.com/AnInLix <oduor.lixir@gmail.com>
2023-02-07 14:58:45 +0300

    added the relevant files for project

---
+++ README.md
@@ -0,0 +1,10 @@
+#json-server with multiple files
+
+#install global
+npm i -g json-server
+
+#install local
+npm install
+
+#Run server
+npm start
---
+++ apis/index.js
@@ -0,0 +1,11 @@
+const buttons = require('./db/buttons.json');
+const slides = require('./db/slides.json');
+const profiles = require('./db/profiles.json');
+const cartegories = require('./db/cartegories.json');
+
+module.exports = () => ({
+	buttons: buttons,
+	slides: slides,
+	profiles, profiles,
+	cartegories, cartegories
+});
